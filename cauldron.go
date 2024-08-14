01f21ea9f18b871034e624e4a88f20a601f73c44
Tom <tomowl@pm.me>
2024-08-14 20:14:37 -0300

    initial commit

---
+++ cauldron.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"status": "ok",
+		})
+	})
+	router.Run()
+}
